4c696f29abcac643b6ca139a7d08bdcd289fc539
harutaka <10650294+harutaka@users.noreply.github.com>
2024-12-31 00:14:07 +0900

    スペース調整

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -9,11 +9,11 @@ type FeatureProps = {
 const Feature = ({ title, src, reverse, detail }: FeatureProps) => {
   return (
     <div className={`md:flex mb-8 ${reverse ? "flex-row-reverse" : ""}`}>
-      <div className="pr-8 pb-4 md:pb-0 md:w-1/2">
+      <div className="pr-0 pb-4 md:pr-8 md:pb-0 md:w-1/2">
         <Image src={src} alt="" width={800} height={600} />
       </div>
 
-      <div className="pr-8 leading-8 md:w-1/2">
+      <div className="pr-0 leading-8 md:pr-8 md:w-1/2">
         <h3 className="pb-4 text-3xl font-bold text-blue-900">{title}</h3>
         <p>{detail}</p>
       </div>

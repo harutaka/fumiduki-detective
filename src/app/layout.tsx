641511c1fdb84324f055f6895543cfbe6d50b3fd
harutaka <10650294+harutaka@users.noreply.github.com>
2025-09-08 16:16:02 +0900

    apply formatter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     url: "https://fumiduki-detective.vercel.app",
     description: "当事務所は浮気調査や素行調査などの調査サービスを提供します。",
     images: [{ url: "https://fumiduki-detective.vercel.app/detective_overview.jpg" }],
-  }
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
